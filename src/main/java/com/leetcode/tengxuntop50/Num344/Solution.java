3d757581ba6d97959eb4ca509bfdabb1e53494d2
htb <tianbaoh.foxmail.com>
2019-11-20 18:10:57 +0800

    编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
    不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
    
    你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。

---
+++ src/main/java/com/leetcode/tengxuntop50/Num344/Solution.java
@@ -0,0 +1,23 @@
+package com.leetcode.tengxuntop50.Num344;
+
+/**
+ * @description:编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+ * <p>
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ * @author: htb
+ * @createDate: 2019/11/13
+ * @version: 1.0
+ */
+public class Solution {
+    public void reverseString(char[] s) {
+        char temp=' ';
+        for (int i=0;i<(s.length)/2;i++){
+            temp=s[i];s[i]=s[s.length-1-i];s[s.length-1-i]=temp;
+        }
+        for (char ch:s
+             ) {
+            System.out.print(ch+" ");
+        }
+
+    }
+}
---
+++ src/main/java/com/leetcode/tengxuntop50/Num344/Test.java
@@ -0,0 +1,16 @@
+package com.leetcode.tengxuntop50.Num344;
+
+/**
+ * @description:
+ * @author: htb
+ * @createDate: 2019/11/14
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        char[] chars={'1','2','3','4','5'};
+        solution.reverseString(chars);
+
+    }
+}
