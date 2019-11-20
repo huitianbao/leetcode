c535936e641fb53255394d92bf62721441590e62
htb <tianbaoh.foxmail.com>
2019-11-20 18:33:54 +0800

    加一些思路的注释

--- src/main/java/com/leetcode/tengxuntop50/Num344/Solution.java
+++ src/main/java/com/leetcode/tengxuntop50/Num344/Solution.java
@@ -11,9 +11,15 @@ package com.leetcode.tengxuntop50.Num344;
 public class Solution {
     public void reverseString(char[] s) {
         char temp=' ';
+        /*
+        反向的排序数组内容
+         */
         for (int i=0;i<(s.length)/2;i++){
             temp=s[i];s[i]=s[s.length-1-i];s[s.length-1-i]=temp;
         }
+        /*
+        遍历输出
+         */
         for (char ch:s
              ) {
             System.out.print(ch+" ");
